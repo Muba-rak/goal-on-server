fb5a0bb7ec0de31a05280836c7b385bd7e4dcaf3
Muba-rak <oladapomubarak@gmail.com>
2024-08-02 18:19:35 +0100

    fixes: prod fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const cors = require("cors");
 const goalRouter = require("./routes/goalRouter");
-const sendMailg = require("./controller/mail");
 
 //middleware
 app.use(express.json());
@@ -13,12 +12,6 @@ app.use(cors());
 
 //route
 app.use("/api/goals", goalRouter);
-app.get("/mail", (req, res) => {
-  res.status(200).send(`<h2>Test email</h2>
-  <a href= '/send'>send mail</a>
-  `);
-});
-app.get("/send", sendMailg);
 
 //db connection
 const start = async () => {
